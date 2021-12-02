0ca852cb756b110aa906f48f7878da65a8e94b48
bhollman1021 <bhollman@uninorte.edu.co>
2021-12-01 23:02:04 -0500

    -

--- pages/RegistroCaso.jsx
+++ pages/RegistroCaso.jsx
@@ -6,7 +6,7 @@ import { useRouter } from "next/router";
 import Geocode from "react-geocode";
 Geocode.setLanguage("en");
 
-Geocode.setApiKey("AIzaSyDKCRIGA2eSIe7ttHfvjDp_HzKWPb3s2BI");
+Geocode.setApiKey("AIzaSyAeZrBfMPk96ZhCyZtwTsAASPBatqVdogw");
 Geocode.setRegion("es");
 
 Geocode.setLocationType("ROOFTOP");
